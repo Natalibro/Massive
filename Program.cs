a70bf39e4bb60575af1b394c8ab0e59417aa1d28
Natalibro <brodka@mail.ru>
2024-03-10 17:25:57 +0300

    task1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+using System.Net.Security;
+using System.Xml.XPath;
+
+string input_string = "Hello my world";
+string result = "";
+string[] temp_array = input_string.Split(" ");
+for(int i = temp_array.Length-1;i >= 0; i--)
+{
+    result += temp_array[i];
+    result += " ";
+} 
+Console.Write(result);
