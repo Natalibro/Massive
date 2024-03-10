b9bba295ad043023903c9a0b26b30b47b0914bb7
Natalibro <brodka@mail.ru>
2024-03-10 18:01:27 +0300

    t4

---
+++ task3/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, 
+//в которой все заглавные буквы заменены на строчные.
+
+string str = "aBcD1tf!-";
+string lowstr = str.ToLower();
+Console.WriteLine(lowstr);
+Console.WriteLine("aBcD1tf!-".ToUpper());
+
+//new
---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+string pal = "кок";
+string time = "Да";
+for(int i = 0; i < pal.Length/2; i++)
+{
+    if(pal[i]!=pal[pal.Length-1-i])
+    {
+        time = "Нет";
+    }
+    Console.Write(time);
+
+}
